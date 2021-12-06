a7f9ca701e3e1bbd09c805fe72281bc75df0c31d
Gaurav-krishna-Galli <gaali.gk19@gmail.com>
2021-12-06 23:10:58 +0530

    SidebarDone

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi! How is it Going???</h1>
+
+      {/* Sidebar  */}
+      <SideBar />
+      {/* Fees  */}
+
+      {/* Widget  */}
+    </div>
+  );
+}
+
+export default App;
